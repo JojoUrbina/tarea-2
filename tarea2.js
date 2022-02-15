963222b42c7d0e4d726f345c0cde43b1e055ebc1
JojoUrbina <95663502+JojoUrbina@users.noreply.github.com>
2022-02-14 23:10:06 -0300

    porque no use variables
    al inicio inclui las variables nombres, apellidos, etc. pero me pareció que se podía automatizar mas.

--- tarea2.js
+++ tarea2.js
@@ -1,12 +1,13 @@
 const $botonsubmit=document.querySelector("#submit")
 $botonsubmit.onclick=function(){
     
-    let nombreCompletoUsuario= document.querySelectorAll(".nombre-completo-usuario")
+    const $elementoNombreCompleto= document.querySelectorAll(".nombre-completo-usuario")
     let nombreCompleto="";
-    for(let i=0;i<nombreCompletoUsuario.length;i++){
-        nombreCompleto+=` ${nombreCompletoUsuario[i].value}`
+    for(let i=0;i<$elementoNombreCompleto.length;i++){
+        nombreCompleto+=` ${$elementoNombreCompleto[i].value}`
     }
-    document.querySelector("h1").innerText=`Bienvenido, ${nombreCompletoUsuario[0].value}`
+    document.querySelector("h1").innerText=`Bienvenido, ${$elementoNombreCompleto[0].value}`
     document.querySelector("#resultado").innerText=nombreCompleto
 }
-
+//primero habia guardado variable por variable(nombre,apellido.etc) pero busque automatizar
+//al maximo.
