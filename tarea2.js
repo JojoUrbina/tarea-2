cbeedbfd438584ab91d5bfb8eee77bc25effc569
Jorman Urbina <95663502+JojoUrbina@users.noreply.github.com>
2022-03-18 12:05:02 -0300

    correciones
    se corrigen los iguales con mas espacios entre el contenido de la variable y la variable

--- tarea2.js
+++ tarea2.js
@@ -1,13 +1,13 @@
 const $botonsubmit=document.querySelector("#submit")
 $botonsubmit.onclick=function(){
     
-    const $elementoNombreCompleto= document.querySelectorAll(".nombre-completo-usuario")
-    let nombreCompleto="";
+    const $elementoNombreCompleto = document.querySelectorAll(".nombre-completo-usuario")
+    let nombreCompleto = "";
     for(let i=0;i<$elementoNombreCompleto.length;i++){
-        nombreCompleto+=` ${$elementoNombreCompleto[i].value}`
+        nombreCompleto +=` ${$elementoNombreCompleto[i].value}`
     }
-    document.querySelector("h1").innerText=`Bienvenido, ${$elementoNombreCompleto[0].value}`
-    document.querySelector("#resultado").innerText=nombreCompleto
+    document.querySelector("h1").innerText = `Bienvenido, ${$elementoNombreCompleto[0].value}`
+    document.querySelector("#resultado").innerText = nombreCompleto
 }
 //primero habia guardado variable por variable(nombre,apellido.etc) pero busque automatizar
 //al maximo.
